6f7050fd221f0dc98a561b0b97e9f50e645320dd
bengroves <bengroves15@gmail.com>
2020-11-22 00:23:30 -0500

    commit tree 149ce3b162378e69f00eec3ea97f0b4c72ede607

---
+++ app/models/show.rb
@@ -0,0 +1,22 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        self.maximum("rating")
+    end
+
+    def self.most_popular_show
+    end
+
+    def self.lowest_rating
+        self.minimum("rating")
+    end
+
+    def self.ratings_sum
+        self.sum("rating")
+    end
+
+    def self.popular_shows
+        self.where( rating  >= 5).pluck(:name)
+    end
+
+end
